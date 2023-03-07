6cdce1bc76ed5a9dfc38fdcd2acdfe268636c18f
EkaterinaFurs <mrs-katrina@mail.ru>
2023-03-07 13:55:51 +0400

    CondionsIfElse

---
+++ example005_Conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("This is Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
